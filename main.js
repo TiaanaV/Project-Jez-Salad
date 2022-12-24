9b679af6644b687652181e8b6682f96e330e7b73
Tatiana Valanitskaya <tatiana010fi@gmail.com>
2022-12-24 16:08:49 +0300

    Update media css

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const navButtonImg = document.querySelector("#nav-button-img");
 
 navButton.onclick = () => {
   if (nav.classList.toggle("open")) {
-    navButtonImg.src = "/assets/menu-close.png";
+    navButtonImg.src = "assets/menu-close.png";
   } else {
-    navButtonImg.src = "/assets/menu-open.png";
+    navButtonImg.src = "assets/menu-open.png";
   }
 };
